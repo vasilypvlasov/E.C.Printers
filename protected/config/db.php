66fbef88aa29125541bdf51f4ebcae832dfbe23a
Dmytro Karpovych <ZAYEC77@gmail.com>
2014-06-15 21:09:52 +0300

    comment fix

--- protected/config/db.php
+++ protected/config/db.php
@@ -3,11 +3,11 @@ $config = [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=printers',
     'username' => 'root',
-    'password' => 'root',
+    //'password' => 'root',
     'charset' => 'utf8',
 ];
 if (defined('YII_DEBUG')) {
-    $config['password'] = 'root';
+    //$config['password'] = 'root';
 }
 
 return $config;
